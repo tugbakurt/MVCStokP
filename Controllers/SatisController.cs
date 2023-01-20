d401237096b578f5037b30acd4d4ff5c1a20a931
tugba kurt <tugba.basdogan@gmail.com>
2023-01-21 02:04:34 +0300

    Popup Modal güncellendi.

--- Controllers/SatisController.cs
+++ Controllers/SatisController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCStok.Models.Entity;
 
 namespace MVCStok.Controllers
 {
     public class SatisController : Controller
     {
         // GET: Satis
+        URUNSTOKPROJEDBEntities db = new URUNSTOKPROJEDBEntities();
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult YeniSatis(TBL_satislar p)
+        {
+            db.TBL_satislar.Add(p);
+            db.SaveChanges();
+            return View("Index");
         }
     }
 }
